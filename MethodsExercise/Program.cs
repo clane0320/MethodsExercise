1eaaca9fb3c910d94942163f0c1bf7432d94b301
Chris Lane <clane0320@gmail.com>
2022-12-08 19:12:20 -0600

    wrote code and story

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            
+            Console.WriteLine("What is your name?");
+            var userName= Console.ReadLine ();
+            
+            Console.WriteLine($"Hey {userName}! What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is my favorite one as well! What is your favorite hobby?");
+            var hobby = Console.ReadLine();
+
+            Console.WriteLine($" No way! {hobby} is mine too! What is your favorite car?");
+            var car = Console.ReadLine();
         }
     }
 }
