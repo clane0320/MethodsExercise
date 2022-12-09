d39aced10d67b30783989507c7efacad44a654b7
Chris Lane <clane0320@gmail.com>
2022-12-08 19:53:43 -0600

    updated exercise to give answers

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-
+            // Excercise 1
             
             Console.WriteLine("What is your name?");
             var userName= Console.ReadLine ();
@@ -15,8 +15,17 @@
             Console.WriteLine($"{color} is my favorite one as well! What is your favorite hobby?");
             var hobby = Console.ReadLine();
 
-            Console.WriteLine($" No way! {hobby} is mine too! What is your favorite car?");
+            Console.WriteLine($"No way! {hobby} is mine too! What is your favorite car?");
             var car = Console.ReadLine();
+
+            Console.WriteLine($"Thanks, {userName}! Here is answers.");
+            Console.WriteLine("-------------------------------------");
+
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"Color: {color}");
+            Console.WriteLine($"Hobby: {hobby}");
+            Console.WriteLine($"Car: {car}");
         }
-    }
-}
+
+
+       
